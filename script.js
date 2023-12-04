3c209b8df504c70fbb44f39219044c4d391b871b
Luis Rizo <rizo.luis@yahoo.com>
2023-12-04 21:43:27 +0100

    Adding javascript findMovies function

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
-function searchClick() {
-  console.log("clicked");
-}
-
 const movieSearchBox = document.getElementById("movie-search-box");
-
-// fetch("http://www.omdbapi.com/?i=tt3896198&apikey=dd3f37db").then((res) =>
-//   res.json().then((data) => console.log(data))
-// );
-
-// http://www.omdbapi.com/?i=tt3896198&apikey=dd3f37db
-
-// poster api http://img.omdbapi.com/?apikey=[dd3f37db]&
-
-async function loadMovies(searchTerm) {
-  const URL = `https://www.omdbapi.com/?s=${searchTerm}&apikey=dd3f37db`;
-  const res = await fetch(`${URL}`);
-  const data = await res.json();
-  //   console.log(data.Search);
-  if (data.Response == "True") displaymovieList(data.Search);
-}
+const movieCard = document.getElementById("movie-card");
 
 function findMovies() {
-  let searchTerm = movieSearchBox.value;
-  console.log(movieSearchBox.value);
+  const searchTerm = movieSearchBox.value;
+  fetch(`http://www.omdbapi.com/?s=${searchTerm}*&apikey=dd3f37db`)
+    .then((response) => response.json())
+    .then((movieResults) => {
+      console.log(movieResults);
+      // Object.keys(json).forEach((movieResult) => {
+      //   const createMovieCard = document.createElement("div");
+      //   createMovieCard.textContent = movieResult.title;
+      //   movieCard.appendChild(createMovieCard);
+      // });
+    });
 }
