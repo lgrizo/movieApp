546f10f56228cde2bc737c99923ebe9e6e9bef2f
Luis Rizo <rizo.luis@yahoo.com>
2024-01-03 00:16:23 -0500

    Fixed function to return movies array

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ const movieCard = document.getElementById("movie-card");
 
 function findMovies() {
   const searchTerm = movieSearchBox.value;
-  fetch(`http://www.omdbapi.com/?s=${searchTerm}*&apikey=dd3f37db`)
-    .then((response) => response.json())
-    .then((movieResults) => {
-      console.log(movieResults);
-      // Object.keys(json).forEach((movieResult) => {
-      //   const createMovieCard = document.createElement("div");
-      //   createMovieCard.textContent = movieResult.title;
-      //   movieCard.appendChild(createMovieCard);
-      // });
-    });
+  if (searchTerm.length > 2) {
+    fetch(`http://www.omdbapi.com/?s=${searchTerm}*&apikey=dd3f37db`)
+      .then((response) => response.json())
+      .then((movieResults) => {
+        console.log(movieResults.Search);
+      })
+      .catch((error) => console.log(error));
+  }
 }
